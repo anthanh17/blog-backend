bdfab46f0d6b3374e523d9c64019ed223d457939
Annt <annguyenthe.work@gmail.com>
2023-08-22 00:26:41 +0700

    init project

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
